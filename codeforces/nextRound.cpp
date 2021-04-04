665c4569d48ffddf42230e863db3fd62cdd65ed1
Prabhat-codes <prabhat_namdharani@yahoo.com>
2021-04-04 20:26:21 +0530

    Next Round

---
+++ codeforces/nextRound.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int no_of_participations;
+    int position_to_check;
+    cin>>no_of_participations>>position_to_check;
+    int i=0;
+    int a[no_of_participations];
+    while(i<no_of_participations){
+        cin>>a[i];
+        i++;
+    }
+    i=0;
+    int counter=0;
+      while(i<no_of_participations){
+        if(a[i]>=a[position_to_check]){
+            counter++;
+        }
+        if(a[i]==0){
+            counter--;
+        }
+        i++;
+    }
+    cout<<counter;
+}
