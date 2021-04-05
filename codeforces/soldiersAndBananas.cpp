2f91cb86e01e96d892c205dff987110615cd6670
Prabhat-codes <prabhat_namdharani@yahoo.com>
2021-04-05 13:03:29 +0530

    soldiersAndBananas

---
+++ codeforces/soldiersAndBananas.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int k,n,w;
+    cin>>k>>n>>w;
+    int total_cost=k*(w*(w+1)/2);
+    if(total_cost<n){
+        cout<<"0";
+    }
+    else
+    cout<<total_cost-n;
+}
