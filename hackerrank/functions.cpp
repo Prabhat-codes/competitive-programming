d78d799e2b57ba8b416a4730a26a3d87873ba26b
Prabhat-codes <prabhat_namdharani@yahoo.com>
2021-04-06 10:58:05 +0530

    Basic C plus plus coding

---
+++ hackerrank/functions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d){
+    int result1=(a>=b)?a:b;
+    int result2=(c>=d)?c:d;
+    int result3=(result1>=result2)?result1:result2;
+    return result3;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
