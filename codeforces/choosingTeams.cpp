feb611fda773b8c7686f4fcbe1f39ab9b286719e
Prabhat-codes <prabhat_namdharani@yahoo.com>
2021-04-13 02:12:04 +0530

    432A Choosing Teams

---
+++ codeforces/choosingTeams.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    int k;
+    cin>>n>>k;
+    int a[n];
+    int counter=0;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        if(a[i]+k<=5){
+            counter++;
+        }
+    }
+    cout<<counter/3;
+}
