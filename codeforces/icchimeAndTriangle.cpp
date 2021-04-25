b17afe93fcbe02724fe8ff4d2aa068515b383784
Prabhat-codes <prabhat_namdharani@yahoo.com>
2021-04-25 19:58:52 +0530

    1337A Icchime And Triangle

---
+++ codeforces/icchimeAndTriangle.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c,d;
+    int n;
+    cin>>n;
+    while(n--){
+    cin>>a>>b>>c>>d;
+    cout<<b<<" "<<c<<" "<<c<<endl;
+    }
+}
