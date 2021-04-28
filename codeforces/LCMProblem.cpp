421c056bf5b28352ce633a895cfbe94e1d179012
Prabhat-codes <prabhat_namdharani@yahoo.com>
2021-04-28 16:49:00 +0530

    1389A LCM Problem

---
+++ codeforces/LCMProblem.cpp
@@ -0,0 +1,16 @@
+ #include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int l,r;
+        cin>>l>>r;
+		if(r<2*l){
+            cout<<"-1 -1\n";
+        }
+        else{
+            cout<<l<<" "<<2*l<<endl;
+        }
+    }
+}
