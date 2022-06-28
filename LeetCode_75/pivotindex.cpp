2d359c8abcf04bad0475b1673c9c41c6f5519b44
Prabhat <prabhat.namdharani@learner.manipal.edu>
2022-06-28 14:24:03 +0530

    Minor change

--- LeetCode_75/pivotindex.cpp
+++ LeetCode_75/pivotindex.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int pivotIndex(vector<int> nums){
+int pivotIndex(vector<int>& nums) {
     int sum1=0;
     for(int i=0;i<nums.size();i++){
         sum1+=nums[i];
@@ -11,15 +11,15 @@ int pivotIndex(vector<int> nums){
     // cout<<"SUM"<<sum1<<endl;
     for(int i=0;i<nums.size();i++){
         int sum=0;
+        int a=accumulate(nums.begin(),nums.begin()+i,sum);
+        int b=sum1-a-nums[i];
         if(a==b){
             return i;
         }
-        int a=a+nums[i];
-        int b=sum1-a-nums[i];
         // cout<<a<<" "<<b<<endl;
     }
     return -1;
-}
+    }
 
 int main() {
    vector<int> a={1,7,3,6,5,6};
